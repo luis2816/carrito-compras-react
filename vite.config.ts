8c018856b9e51b6fa7acce84c7a3bf8348789e52
luis2816 <ordonezfelipe2816@gmail.com>
2025-03-07 10:34:21 -0500

    Ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',  
     port: 10000,  
-    strictPort: true
+    strictPort: true,
+    allowedHosts: ['carrito-compras-react.onrender.com']  // Agregar el dominio de Render
   }
 });
