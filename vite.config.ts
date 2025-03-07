2bbfdefadab8f9fde76db4b29b1e337cfa02256a
luis2816 <ordonezfelipe2816@gmail.com>
2025-03-07 10:30:48 -0500

    Ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Permite acceso desde fuera del localhost
+    port: 10000,      // Usa el puerto que Render asignará
+    strictPort: true  // Evita que Vite cambie de puerto si está ocupado
+  },
   preview: {
-    allowedHosts: ['carrito-compras-react.onrender.com']
+    host: '0.0.0.0',  
+    port: 10000,  
+    strictPort: true
   }
 });
