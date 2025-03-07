81a86bde2aa8e3de6d03be04a2165699d466a131
luis2816 <ordonezfelipe2816@gmail.com>
2025-03-07 10:17:23 -0500

    Ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Permite conexiones externas
-    allowedHosts: ['.ngrok-free.app'], // Permite ngrok
-    port: 5173 // Asegura que usa el puerto correcto
+  preview: {
+    allowedHosts: ['carrito-compras-react.onrender.com']
   }
-})
+});
